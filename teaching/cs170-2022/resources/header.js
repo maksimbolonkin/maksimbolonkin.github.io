9988f22bf5d6e11e9252908c97d886ab98b03b1f
Maksim Bolonkin <maksimbolonkin@gmail.com>
2022-09-07 15:47:42 -0400

    Added materials for 2002 class

---
+++ teaching/cs170-2022/resources/header.js
@@ -0,0 +1,35 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="index.html">CS170</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="#overview">О курсе</a></li>\
+              <li><a href="#syllabus">Материалы</a></li>\
+              <li><a href="#homeworks">Домашние задания</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="https://msu.uz/">\
+    <img src="resources/logo-mm.png" class="logo-right">\
+  </a>\
+  <h1>CS 170: Оптимизация и численные методы</h1>\
+  <h3>Филиал МГУ им. М.В. Ломоносова в г. Ташкенте, осенний семестр 2022 г.</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
